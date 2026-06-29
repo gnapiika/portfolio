141a2d4ad96c2e35d86f9b810797f6e3421d8533
gnapika <gnapiika@gmail.com>
2026-06-29 23:54:43 +0530

    Update Experience.tsx

--- components/sections/Experience.tsx
+++ components/sections/Experience.tsx
@@ -4,7 +4,7 @@ export default function Experience() {
   return (
     <Section id="experience" title="Experience">
       <p className="text-gray-400">
-        Internships, research, and achievements will appear here.
+        I'm open to gain experience in the field of cybersecurity, artificial intelligence, data science, and full stack development. I am actively seeking internships and opportunities to apply my skills in real-world projects. If you have any opportunities or would like to collaborate, please feel free to reach out to me.
       </p>
     </Section>
   );
